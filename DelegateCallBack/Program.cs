c1ad0f713241501d1e62ec4af53fe22d0981e862
Amit Upadhyay <Amit.Upadhyay@Honeywell.com>
2019-09-25 06:00:43 +0530

    CallBackDelegate

---
+++ DelegateCallBack/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateCallBack
+{
+    public delegate bool checkEligbility(Emp e);
+    public class Emp
+    {
+        public string Name;
+        public int Sal;
+        public void CalculatePromAtion(List<Emp> obj,checkEligbility objdelgate)
+        {
+            foreach (Emp e in obj)
+            {
+                if (objdelgate(e))
+                {
+                    Console.WriteLine("Eilgable");
+                }
+            }
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Emp> obj = new List<Emp>();
+            obj.Add(new Emp { Name = "Amit", Sal = 100 });
+            Emp obj1 = new Emp();
+            checkEligbility delobj = new checkEligbility(IsPromateable);
+            obj1.CalculatePromAtion(obj, delobj);
+        }
+       static bool IsPromateable(Emp e)
+        {
+         if(e.Sal>5)
+            {
+                Console.WriteLine("IsPromte");
+            }
+            return true;
+        }
+    }
+}
---
+++ print/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace print
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int res = 0;
+            int number = 1792;
+            res = DoOperation(number);
+           
+                if(res>=10)
+                {
+                while (res >= 10)
+                {
+                    number = res;
+                    res = DoOperation(number);
+                }
+                }
+                 
+            
+            
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+        static int DoOperation(int n)
+        {
+            int Sum = 0;
+            while (n > 0)
+            {
+                Sum = Sum + (n % 10);
+                n = n / 10;
+            }
+            return Sum;
+        }
+    }
+}
